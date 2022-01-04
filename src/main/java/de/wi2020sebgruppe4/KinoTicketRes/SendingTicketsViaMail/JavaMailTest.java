342a788dd437f00f812e84df23135ae5dc7175d5
juliusw01 <95189236+juliusw01@users.noreply.github.com>
2022-01-04 17:52:28 +0100

    Es können Bestätigungsmails verschickt werden

---
+++ src/main/java/de/wi2020sebgruppe4/KinoTicketRes/SendingTicketsViaMail/JavaMailTest.java
@@ -0,0 +1,13 @@
+package de.wi2020sebgruppe4.KinoTicketRes.SendingTicketsViaMail;
+
+public class JavaMailTest {
+
+    public static void main(String[] args) {
+        try {
+            JavaMail.sendMail("noreply.kinores@gmail.com");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
